aae6ad0c5a938269cba0fc5259021b56673e8b01
yellowsea <yunhai0644@163.com>
2023-07-08 14:54:21 +0800

    feat✨:  add zustand  store

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,17 @@
 
+import useCounterStore from "@/stores/counter"
+
 const Home: React.FC = () => {
+
+  const counter = useCounterStore((state) => state.counter)
+  const increase = useCounterStore((state) => state.increase)
+
   return (
     <div>
       <h1> Home Page </h1>
+      <button onClick={() => increase(1)}>
+        counter: {counter}
+      </button>
     </div>
   )
 }
---
+++ src/stores/counter.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand"
+
+interface CounterState {
+  counter: number
+  increase: (by: number) => void
+}
+
+const useCounterStore = create<CounterState>()((set) => ({
+  counter: 0,
+  increase: (by) => set((state) => ({ counter: state.counter + by }))
+}))
+
+
+export default useCounterStore
